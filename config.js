b8ffebbfe5f52a4ccf4505254ff373b4a4474fc3
Phạm Cảnh Dinh <dinhpc@rikkeisoft.com>
2022-11-19 18:15:34 +0700

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+const nameGirl = "Chị";
+const giftUrl = "https://www.facebook.com/ngoc.nguyen1911";
+const eventName = "Chúc mừng sinh nhật";
+const titleCard = "Tặng chị Ngọc";
+const contentCard =
+  "Chúc chị sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị trong hôm nay và cả những ngày sau";
+const giftImage = "anh1.jpg";
